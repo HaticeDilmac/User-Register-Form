35ba856ea9df846070151fe6ef1fb3e688c581a8
HaticeDilmac <haticedilmac.1011@gmail.com>
2024-12-09 00:27:34 +0300

    Initial Commit

---
+++ User Register Form/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  User Register Form
+//
+//  Created by hatice on 9.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ User Register FormTests/User_Register_FormTests.swift
@@ -0,0 +1,17 @@
+//
+//  User_Register_FormTests.swift
+//  User Register FormTests
+//
+//  Created by hatice on 9.12.2024.
+//
+
+import Testing
+@testable import User_Register_Form
+
+struct User_Register_FormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
